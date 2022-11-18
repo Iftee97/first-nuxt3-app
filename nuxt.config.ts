de35d7093cc0dd5caee3ca5ecae05722a3f3f6d5
Iftee97 <tawsifmasrur@gmail.com>
2022-11-18 19:24:29 +0600

    added things to the nuxt.config.ts file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
 
+  app: {
+    head: {
+      title: 'Nuxt Dojo',
+      meta: [
+        { name: 'description', content: 'Everything about Nuxt3' },
+      ],
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
+      ]
+    },
+  },
+
   tailwindcss: {
     cssPath: '~/assets/css/tailwind.css',
   }
