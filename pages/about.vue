dadbe9ba9a5136be6d18d17502a2d6bb97623041
Iftee97 <tawsifmasrur@gmail.com>
2022-11-18 17:41:06 +0600

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+      delectus iusto quidem quo id adipisci quas porro libero harum! Nisi!
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+      delectus iusto quidem quo id adipisci quas porro libero harum! Nisi!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
