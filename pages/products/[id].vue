e7b458267739fad1eeef74d3ccdc9e35165e9438
Iftee97 <tawsifmasrur@gmail.com>
2022-11-18 17:45:04 +0600

    added dynamic routes

---
+++ pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>
+      Product Details for <strong>{{ id }}</strong>
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae atque
+      reprehenderit eum repellendus hic, saepe modi in itaque praesentium alias.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
