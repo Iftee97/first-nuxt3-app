88d43879155036fd04f8d9e051bdc249a1f9734b
Iftee97 <tawsifmasrur@gmail.com>
2022-11-21 09:26:45 +0600

    added title and meta tags and values

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -14,4 +14,9 @@ definePageMeta({
 }); // add products layout in this page
 
 const { data: products } = await useFetch("https://fakestoreapi.com/products"); // fetch data from API
+
+useHead({
+  title: "Nuxt Dojo | Merch",
+  meta: [{ name: "description", content: "Nuxt 3 Merch" }],
+}); // add title & meta tags in this page
 </script>
